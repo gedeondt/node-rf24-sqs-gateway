6a6c31cc66c8cabf0c44f10eda1114fbae5d8818
linaro <gedeondt@gmail.com>
2021-04-18 07:40:36 +0200

    terraform outputs

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -5,3 +5,12 @@ resource "aws_iam_user" "gateway" {
 resource "aws_iam_access_key" "gateway" {
   user = aws_iam_user.gateway.name
 }
+
+output "gateway_key_id" {
+  value = aws_iam_access_key.gateway.id
+}
+
+output "gateway_key_key" {
+  value = aws_iam_access_key.gateway.secret
+  sensitive   = true
+}
