a594177a506e19e1867e0929e6bf9fc127a70edd
Gedeón Domínguez <gedeon.dominguez@clouddistrict.com>
2021-04-20 10:49:45 +0200

    Credentials v0.1

--- terraform/lambda/credentials.js
+++ terraform/lambda/credentials.js
@@ -2,26 +2,35 @@
 const AWS = require('aws-sdk');
 
 exports.handler = function (event, context, callback) {
-  var response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html; charset=utf-8',
-    },
-    body: '<h2>Hello world...Welcome to AWS...</h2>',
-  }
+  
+  const savedPasswords = JSON.parse(process.env.PASSWORDS);
+  const requestPassword = JSON.parse(event.body);
 
-  var roleToAssume = {RoleArn: process.env.ROLE_ARN,
+  if(savedPasswords[requestPassword['user']] &&
+     savedPasswords[requestPassword['user']] == requestPassword.password)
+  {
+      var roleToAssume = {RoleArn: process.env.ROLE_ARN,
                       RoleSessionName: 'session1',
                       DurationSeconds: 900,};
-  var roleCreds;
-  
-  // Create the STS service object    
-  var sts = new AWS.STS({apiVersion: '2011-06-15'});
-  
-  //Assume Role
-  sts.assumeRole(roleToAssume, function(err, data) {
-      console.log(err, data);
-      callback(null, response);
-  });
-  
+      var roleCreds;
+      
+      // Create the STS service object    
+      var sts = new AWS.STS({apiVersion: '2011-06-15'});
+      
+      //Assume Role
+      sts.assumeRole(roleToAssume, function(err, data) {
+          console.log(err, data);
+            callback(null, {
+              statusCode: 200,
+              headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+              },
+              body: JSON.stringify(data.Credentials),
+            });
+      });
+  }
+  else
+  {
+    callback(null, { statusCode: 401, body: JSON.stringify(savedPasswords)+JSON.stringify(requestPassword)});
+  }
 }
